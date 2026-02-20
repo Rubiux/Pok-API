8734598c0c4561e226c9a0f08e15794220513965
Rubiux <byrubius10@gmail.com>
2026-02-20 09:19:05 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,31 +10,46 @@ createApp({
       selectedPokemon: {},
       currentPage: 1,
       perPage: 20,
-      loading: true
+      loading: true,
+
+      newPokemon: {
+        name: '',
+        image: ''
+      }
     }
   },
 
   mounted() {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=1302')
-      .then(res => res.json())
-      .then(data => {
 
-        this.pokemonList = data.results.map((p, index) => {
-          const id = index + 1;
+    const savedList = localStorage.getItem('pokemonList');
 
-          return {
-            id: id,
-            name: p.name,
-            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-          };
+    if (savedList) {
+      this.loadFromLocalStorage();
+      this.loading = false;
+    } else {
+
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=1302')
+        .then(res => res.json())
+        .then(data => {
+
+          this.pokemonList = data.results.map((p, index) => {
+            const id = index + 1;
+
+            return {
+              id: id,
+              name: p.name,
+              image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+            };
+          });
+
+          this.saveToLocalStorage();
+          this.loading = false;
+        })
+        .catch(error => {
+          console.error("Error:", error);
+          this.loading = false;
         });
-
-        this.loading = false;
-      })
-      .catch(error => {
-        console.error("Error:", error);
-        this.loading = false;
-      });
+    }
   },
 
   computed: {
@@ -83,15 +98,91 @@ createApp({
   },
 
   watch: {
+
     search() {
       this.currentPage = 1;
+      this.saveToLocalStorage();
     },
+
     searchId() {
       this.currentPage = 1;
+      this.saveToLocalStorage();
+    },
+
+    currentPage() {
+      this.saveToLocalStorage();
+    },
+
+    pokemonList: {
+      handler() {
+        this.saveToLocalStorage();
+      },
+      deep: true
     }
+
   },
 
   methods: {
+
+    saveToLocalStorage() {
+      localStorage.setItem('pokemonList', JSON.stringify(this.pokemonList));
+      localStorage.setItem('search', this.search);
+      localStorage.setItem('searchId', this.searchId);
+      localStorage.setItem('currentPage', this.currentPage);
+    },
+
+    loadFromLocalStorage() {
+      const savedList = localStorage.getItem('pokemonList');
+      const savedSearch = localStorage.getItem('search');
+      const savedSearchId = localStorage.getItem('searchId');
+      const savedPage = localStorage.getItem('currentPage');
+
+      if (savedList) {
+        this.pokemonList = JSON.parse(savedList);
+      }
+
+      if (savedSearch) {
+        this.search = savedSearch;
+      }
+
+      if (savedSearchId) {
+        this.searchId = savedSearchId;
+      }
+
+      if (savedPage) {
+        this.currentPage = parseInt(savedPage);
+      }
+    },
+
+    addPokemon() {
+
+      if (!this.newPokemon.name) {
+        alert("El nombre es obligatorio");
+        return;
+      }
+
+      const maxId = this.pokemonList.length > 0
+        ? Math.max(...this.pokemonList.map(p => p.id))
+        : 0;
+
+      const newId = maxId + 1;
+
+      const pokemon = {
+        id: newId,
+        name: this.newPokemon.name.toLowerCase(),
+        image: this.newPokemon.image || 
+          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'
+      };
+
+      this.pokemonList.push(pokemon);
+
+      this.newPokemon.name = '';
+      this.newPokemon.image = '';
+
+      this.saveToLocalStorage();
+
+      alert("Pokémon agregado correctamente 🔥");
+    },
 
     showDetail(poke) {
 
@@ -112,12 +203,28 @@ createApp({
           );
 
           modal.show();
+        })
+        .catch(() => {
+          this.selectedPokemon = poke;
+
+          const modal = new bootstrap.Modal(
+            document.getElementById('detailModal')
+          );
+
+          modal.show();
         });
     },
 
     deletePokemon(id) {
       this.pokemonList =
         this.pokemonList.filter(p => p.id !== id);
+
+      this.saveToLocalStorage();
+    },
+
+    clearStorage() {
+      localStorage.clear();
+      location.reload();
     }
 
   }
